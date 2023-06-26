d0b2385da8bb6b7906e2d2b0753a059bdffa38b5
Bill Garrett <garrett@astralfrontier.org>
2023-06-25 18:14:52 -0700

    Support quality picker

--- src/sheetworkers/rollers.ts
+++ src/sheetworkers/rollers.ts
@@ -2,6 +2,10 @@
 
 // Documentation: https://wiki.roll20.net/Sheet_Worker_Scripts
 
+function attrname(prefix: string, name: string) {
+  return `${prefix}_${name.toLowerCase().replace(' ', '_')}`
+}
+
 function propsToRollTemplate(
   template: string,
   props: Record<string, string>
@@ -19,12 +23,11 @@ function finishSkillRoll(outcome: StartRollCallbackValues) {
   } = outcome
 
   const [_dieroll, tn_s] = expression.split('+')
+
+  // Count successes
   const tn = parseInt(tn_s) || 6
-
-  // TODO: how many dice were at or under TN?
-
-  const successes = dice.filter((die) => die <= tn).length
-  const result = successes ? `Success vs TN ${tn}` : `Failure vs TN ${tn}`
+  const successes = dice.filter((die) => die <= tn)
+  const result = `${successes.length ? 'Success' : 'Failure'} vs TN ${tn}`
 
   finishRoll(rollId, {
     roll: dice.map((s: any) => s.toString()).join(' '),
@@ -35,24 +38,33 @@ function finishSkillRoll(outcome: StartRollCallbackValues) {
 function startSkillRoll(
   skillname: string,
   skillrank: string,
-  qualities: string
+  qualities: string[]
 ) {
-  // TODO: custom template
+  let quality_query
+  if (qualities.length == 1) {
+    quality_query = `@{${attrname('quality', qualities[0])}}`
+  } else {
+    const options = qualities.map(
+      (name) => `${name},@{${attrname('quality', name)}}`
+    )
+    quality_query = `?{Quality|${options.join('|')}}`
+  }
+  const roll_s = `[[${skillrank}d${quality_query}+?{TN|6}]]`
+
   const template = propsToRollTemplate('check', {
     name: skillname,
-    roll: `[[${skillrank}d${qualities}+?{TN|6}]]`,
+    roll: roll_s,
     result: '[[0]]',
   })
   startRoll(template, finishSkillRoll)
 }
 
 on('clicked:skill', (event) => {
-  const attr = event.htmlAttributes['data-skillname']
-  const qualities = event.htmlAttributes['data-qualities']
-
-  // TODO: skills with multiple qualities is buggy
+  const skillname: string = event.htmlAttributes['data-skillname'] || ''
+  const attr = attrname('skill', skillname)
+  const qualities: string[] = event.htmlAttributes['data-qualities'].split('|')
 
   getAttrs([attr], (v) => {
-    startSkillRoll(attr, v[attr], qualities)
+    startSkillRoll(skillname, v[attr], qualities)
   })
 })
